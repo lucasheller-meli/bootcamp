3053126f93ed140c3ffd6aa57d73eae05db5234e
Marianne Bianca <mariannebiancamb@gmail.com>
2021-05-20 12:26:37 -0300

    Exercio de mari 0.1

---
+++ src/com/company/Execircio2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Execircio2 {
+
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digita o numero n:");
+        int n = teclado.nextInt();
+
+    }
+}
