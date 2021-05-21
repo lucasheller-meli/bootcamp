51ae0b26f4f91a04d9cce6385dcf789c9ab9307c
Marianne Bianca <mariannebiancamb@gmail.com>
2021-05-21 07:32:01 -0300

    Exercicio 4 e radixRascunho de mari

---
+++ src/com/company/ExercicioArray.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ExercicioArray {
+
+    public static void main(String[] args){
+        Integer[] array = {52, 10, 05, 1324, 01, 13, 62, 30, 12, 127};
+
+        Arrays.sort(array);
+        System.out.println(array);
+
+    }
+}
