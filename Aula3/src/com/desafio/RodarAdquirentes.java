e8649335546ba69be808b7ad3c29fa133a9a62b4
Lucas Heller <lucas.heller@mercadolivre.com>
2021-05-24 17:15:10 -0300

    Alterar variavel

--- Aula3/src/com/desafio/RodarAdquirentes.java
+++ Aula3/src/com/desafio/RodarAdquirentes.java
@@ -2,9 +2,9 @@ package com.desafio;
 
 public class RodarAdquirentes {
     public static void main(String[] args) {
-        Adquirentes cielo = new Cielo();
+        final Adquirentes cielo = new Cielo();
         System.out.println(cielo.verificaTaxa());
-        Adquirentes stone = new Stone();
+        final Adquirentes stone = new Stone();
         System.out.println(stone.verificaTaxa());
     }
 }
